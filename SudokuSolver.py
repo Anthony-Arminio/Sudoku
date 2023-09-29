04c46f7ad09a1c001af4090cc41275c5a749d81a
Sal Arminio <tonyarminio.1@gmail.com>
2023-09-29 09:59:39 -0400

    functional for puzzle 6

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -22,12 +22,22 @@ class Agent():
         while not self.stop:
             self.stop = True
             for cell in range(81):
-                self.findValidOptions(deepcopy(self.grid), cell)
+                self.reduceOptions(cell)
+
+        self.stop = False
+        while not self.stop:
+            gridCopy = deepcopy(self.grid)
+            self.stop = True
+            for cell in range(81):
+                for value in gridCopy[cell]:
+                    if not self.isValid(deepcopy(gridCopy), cell, value):
+                        self.grid[cell].remove(value)
+                        self.stop = False
         print("")
         print(self.grid)
     
-    def findValidOptions(self, grid, cell):
-        options = copy(grid[cell])
+    def reduceOptions(self, cell):
+        options = copy(self.grid[cell])
         for value in options:
             for testCell in range(81):
                 if self.sees(cell, testCell):
@@ -35,6 +45,26 @@ class Agent():
                         self.grid[cell].remove(value)
                         self.stop = False
                         break
+    
+    def isValid(self, grid, cell, value): # don't use self.grid in this function
+        if self.isContradiction(grid, cell, value):
+            return False
+        newGrid = copy(grid)
+        newGrid[cell] = [value]
+        if cell + 1 < 81:
+            for value in newGrid[cell + 1]:
+                if self.isValid(copy(newGrid), cell + 1, value):
+                    return True
+            return False
+        return True
+
+
+    def isContradiction(self, grid, cell, value):
+        for testCell in range(81):
+            if self.sees(cell, testCell):
+                if len(grid[testCell]) == 1 and grid[testCell][0] == value:
+                    return True
+
 
     def sees(self, cell1, cell2):
         if cell1 == cell2:
